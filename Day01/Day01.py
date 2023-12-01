3da4187a0bdd8577df22aaf5a43113a44cb86571
Sam Lee <sam.lee@n-able.com>
2023-12-01 13:53:13 +0000

    Day01 Soln

---
+++ Day01/Day01.py
@@ -0,0 +1,29 @@
+import re
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+# Part 1
+total = 0 
+for line in lines:
+  num = re.sub('\D', '', line)
+  total += int(num[0]+num[-1])
+#   print(line, int(num[0]+num[-1]), total)
+print('Part 1:', total)
+
+# Part 2
+num_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,'seven':7,'eight':8,'nine':9,}
+total = 0 
+for line in lines:
+    new_line = ''
+    for i in range(len(line)):
+        if line[i].isdigit():
+            new_line += line[i]
+        else:
+            for key, value in num_dict.items():
+                if re.match(f"^{key}", line[i:]):
+                    new_line += str(value)
+    num = re.sub('\D', '', line)
+    total += int(new_line[0]+new_line[-1])
+#   print(line, int(num[0]+num[-1]), total)
+print('Part 2:', total)
