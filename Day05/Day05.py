d290cda873cfcb7334a61e720e7b079727dd9230
Sam Lee <sam.lee@n-able.com>
2023-12-05 20:08:01 +0000

    Day05 format

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,6 +1,7 @@
 import re
 import time
 from collections import defaultdict
+
 start = time.time()
 with open("input.txt") as f:
     lines = f.readlines()
@@ -13,7 +14,7 @@ maps = defaultdict(list)
 for line in lines:
     if "-to-" in line:
         map_type = line.split(" ")[0]
-    
+
     if re.match(r"\d+", line):
         maps[map_type].append([int(x) for x in re.findall(r"\d+", line)])
 
@@ -21,21 +22,21 @@ for key in maps.keys():
     for i in range(len(seeds)):
         remapped = False
         for converter in maps[key]:
-            if converter[1] <= seeds[i] < converter[1] + converter[2]+1:
+            if converter[1] <= seeds[i] < converter[1] + converter[2] + 1:
                 seeds[i] += converter[0] - converter[1]
                 remapped = True
                 break
         if not remapped:
             seeds[i]
 
-print('Part 1:', min(seeds))
+print("Part 1:", min(seeds))
 
 # Part 2
 seeds_pair = []
 loc_pair = []
 seeds = [int(x) for x in re.findall(r"\d+", lines[0])]
 for i in range(0, len(seeds), 2):
-    seeds_pair.append([seeds[i] , seeds[i+1] + seeds[i]]) 
+    seeds_pair.append([seeds[i], seeds[i + 1] + seeds[i]])
 
 for key in maps.keys():
     next_seeds_pair = []
@@ -45,7 +46,10 @@ for key in maps.keys():
             offset = converter[0] - converter[1]
             if seed[1] < converter[1] or seed[0] > converter[1] + converter[2]:
                 continue
-            new_range = [max(seed[0], converter[1]), min(seed[1], converter[1] + converter[2])]
+            new_range = [
+                max(seed[0], converter[1]),
+                min(seed[1], converter[1] + converter[2]),
+            ]
             left_gap = [seed[0], new_range[0]]
             right_gap = [new_range[1], seed[1]]
             if left_gap[1] > left_gap[0]:
@@ -59,5 +63,4 @@ for key in maps.keys():
         seeds_pair = next_seeds_pair
     # print("End:", seeds_pair, key)
 
-print('Part 2:', min([ x[0] for x in seeds_pair]))
-
+print("Part 2:", min([x[0] for x in seeds_pair]))
